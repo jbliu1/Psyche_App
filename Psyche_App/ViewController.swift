00cfee043cb05770341ed9cfe4fca57d77db4676
juboliu <julialiu@Julias-MacBook.local>
2017-11-08 01:41:39 -0700

    Initial Commit

---
+++ Psyche_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psyche_App
+//
+//  Created by Julia Liu on 11/8/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
